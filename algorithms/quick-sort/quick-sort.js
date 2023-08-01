2f0628691443e9b9f1a15ffaeca091c8fa3bc790
Vladimir <fruityflunt@gmail.com>
2023-08-01 09:43:50 +0300

    Quick Sort

---
+++ algorithms/quick-sort/quick-sort.js
@@ -0,0 +1,24 @@
+"use strict";
+const quickSort = (array) => {
+    if (array.length <= 1) {
+        return array;
+    }
+    const pivotIndex = Math.floor(array.length / 2);
+    const pivot = array[pivotIndex];
+    const less = [];
+    const greater = [];
+    for (let i = 0; i < array.length; i++) {
+        if (pivotIndex === i)
+            continue;
+        if (array[i] < pivot) {
+            less.push(array[i]);
+        }
+        else {
+            greater.push(array[i]);
+        }
+    }
+    return [...quickSort(less), pivot, ...quickSort(greater)];
+};
+const unsortedQuickSortArray = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+const sortedQuickSortArray = quickSort(unsortedQuickSortArray);
+console.log(sortedQuickSortArray);
