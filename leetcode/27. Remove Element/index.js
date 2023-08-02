064bd86cee32b0fe279462fb222c1e89cf064074
Vladimir <fruityflunt@gmail.com>
2023-08-02 09:57:03 +0300

    Скрыл JS файлы от гита

--- leetcode/27. Remove Element/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function removeElement(nums, val) {
-    const newArray = nums.filter((num) => num !== val);
-    nums.length = 0;
-    for (let i = 0; i < newArray.length; i++) {
-        nums.push(newArray[i]);
-    }
-    return newArray.length;
-}
--- leetcode/9. Palindrome Number/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Given an integer x, return true if x is a palindrome, and false otherwise.
-// Example 1:
-// Input: x = 121
-// Output: true
-// Explanation: 121 reads as 121 from left to right and from right to left.
-function isPalindrome(x) {
-    return x.toString() === x.toString().split("").reverse().join("");
-}
