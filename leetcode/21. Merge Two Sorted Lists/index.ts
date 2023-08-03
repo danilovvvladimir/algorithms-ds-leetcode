fd4947353396a50617aa5db2e1ed4b86788c9b86
Vladimir <fruityflunt@gmail.com>
2023-08-03 08:47:58 +0300

    Leetcode: 83

--- leetcode/21. Merge Two Sorted Lists/index.ts
+++ leetcode/21. Merge Two Sorted Lists/index.ts
@@ -1,54 +1,50 @@
-class ListNode {
-  val: number;
-  next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
+namespace NS21 {
+  class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+      this.val = val === undefined ? 0 : val;
+      this.next = next === undefined ? null : next;
+    }
   }
-}
 
-function mergeTwoLists(
-  list1: ListNode | null,
-  list2: ListNode | null
-): ListNode | null {
-  let currentNode = new ListNode();
-  const headNode = currentNode;
+  function mergeTwoLists(
+    list1: ListNode | null,
+    list2: ListNode | null
+  ): ListNode | null {
+    let currentNode = new ListNode();
+    const headNode = currentNode;
 
-  while (list1 && list2) {
-    if (list1.val < list2.val) {
+    while (list1 && list2) {
+      if (list1.val < list2.val) {
+        currentNode.next = list1;
+        list1 = list1.next;
+      } else {
+        currentNode.next = list2;
+        list2 = list2.next;
+      }
+      currentNode = currentNode.next;
+    }
+
+    if (list1) {
       currentNode.next = list1;
-      list1 = list1.next;
     } else {
       currentNode.next = list2;
-      list2 = list2.next;
     }
-    currentNode = currentNode.next;
-  }
 
-  if (list1) {
-    currentNode.next = list1;
-  } else {
-    currentNode.next = list2;
+    return headNode.next;
   }
-
-  return headNode.next;
 }
 
-const List1Node1 = new ListNode(4);
-const List1Node2 = new ListNode(2, List1Node1);
-const List1Node3 = new ListNode(1, List1Node2);
+// Нам нужен новый список, поэтому сначала создаём пустую ноду.
+// После этого создаем ещё одну переменную, которая будет указывать на начало текущего списка.
 
-const List2Node1 = new ListNode(4);
-const List2Node2 = new ListNode(3, List2Node1);
-const List2Node3 = new ListNode(1, List2Node2);
+// Потом в цикле, пока существуют первый и второй список мы будем проверять.
+// Проверять значения (сверяем значения нод в списках), в итоговый список будем привязывать ту ноду, чье значение меньше.
+// И в том списке, где взяли эту ноду, будем перемещаться к следующей ноде.
 
-// Input: (list1 = [1, 2, 4]), (list2 = [1, 3, 4]);
-// Output: [1, 1, 2, 3, 4, 4];
+// Таким образом мы всегда будем добавлять наименьший элемент при проверке.
 
-let finalList = mergeTwoLists(List1Node3, List2Node3);
+// И потом после цикла вводим ещё одну проверку, если какой-то список не равен null, то добавляем его к итоговому.
 
-while (finalList) {
-  console.log(finalList.val);
-
-  finalList = finalList.next;
-}
+// Возвращаем следующий элемент с головы итогового списка, т.к самый первый элемент в нём - дефолтный.
---
+++ leetcode/83. Remove Duplicates from Sorted List/index.ts
@@ -0,0 +1,35 @@
+namespace NS83 {
+  class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+      this.val = val === undefined ? 0 : val;
+      this.next = next === undefined ? null : next;
+    }
+  }
+
+  function deleteDuplicates(head: ListNode | null): ListNode | null {
+    if (head === null) return null;
+
+    let currentNode = head;
+
+    while (currentNode.next !== null) {
+      if (currentNode.val === currentNode.next.val) {
+        currentNode.next = currentNode.next.next;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    return head;
+  }
+}
+
+// Сначала проверяем на то, равна ли нода null, если так, то и вернём null.
+
+// Вводим переменную, которая будет обозначать текущую ноду. Инициализируем головой.
+
+// Потом в цикле (пока есть следующая нода, т.е пробегаемся по всему листу),
+// смотрим на то, равно ли текущее значение следующему.
+// Если да, то мы должны следующий скипнуть (перепривязываем - убираем следующую ноду которая повторяется).
+// Если нет, то всё норм и дальше продолжаем проверять.
