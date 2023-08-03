36a50424bfa13068472e78c9cf176022bf46f597
Vladimir <fruityflunt@gmail.com>
2023-08-03 07:01:17 +0300

    Leetcode: 160

---
+++ leetcode/160. Intersection of Two Linked Lists/index.ts
@@ -0,0 +1,49 @@
+namespace NS160 {
+  class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+      this.val = val === undefined ? 0 : val;
+      this.next = next === undefined ? null : next;
+    }
+  }
+
+  function getIntersectionNode(
+    headA: ListNode | null,
+    headB: ListNode | null
+  ): ListNode | null {
+    let newHeadA = headA;
+    let newHeadB = headB;
+
+    while (newHeadA != newHeadB) {
+      if (newHeadA === null) {
+        newHeadA = headB;
+      } else {
+        newHeadA = newHeadA.next;
+      }
+
+      if (newHeadB === null) {
+        newHeadB = headA;
+      } else {
+        newHeadB = newHeadB.next;
+      }
+    }
+
+    return newHeadA;
+  }
+
+  const List1Node5 = new ListNode(5);
+  const List1Node4 = new ListNode(4, List1Node5);
+  const List1Node3 = new ListNode(8, List1Node4);
+  const List1Node2 = new ListNode(1, List1Node3);
+  const List1Node1 = new ListNode(4, List1Node2);
+
+  const List2Node6 = new ListNode(5);
+  const List2Node5 = new ListNode(4, List2Node6);
+  const List2Node4 = new ListNode(8, List2Node5);
+  const List2Node3 = new ListNode(1, List2Node4);
+  const List2Node2 = new ListNode(6, List2Node3);
+  const List2Node1 = new ListNode(5, List2Node2);
+
+  console.log(getIntersectionNode(List1Node1, List2Node1));
+}
