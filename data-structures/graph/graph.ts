22d996d635b868a25831591b73dfad7ead407060
Vladimir <fruityflunt@gmail.com>
2023-08-04 10:15:55 +0300

    Graph, DFS, BFS

---
+++ data-structures/graph/graph.ts
@@ -0,0 +1,91 @@
+namespace NSGraph {
+  class Graph {
+    private adjacencyList: Map<string, string[]>;
+
+    constructor() {
+      this.adjacencyList = new Map();
+    }
+
+    addNode(node: string) {
+      if (!this.adjacencyList.has(node)) {
+        this.adjacencyList.set(node, []);
+      }
+    }
+
+    addEdge(fromNode: string, toNode: string) {
+      if (
+        !this.adjacencyList.has(fromNode) ||
+        !this.adjacencyList.has(toNode)
+      ) {
+        throw new Error("Both nodes should exist in the graph.");
+      }
+
+      this.adjacencyList.get(fromNode)?.push(toNode);
+    }
+
+    getNeighbors(node: string): string[] | undefined {
+      return this.adjacencyList.get(node);
+    }
+
+    getAll() {
+      return this.adjacencyList;
+    }
+
+    printAdjacencyList() {
+      for (const [node, neighbors] of this.adjacencyList) {
+        console.log(`${node} -> ${neighbors.join(", ")}`);
+      }
+    }
+
+    printDFS(sourceNodeValue: string) {
+      const stack = [sourceNodeValue];
+
+      while (stack.length > 0) {
+        const current = stack.pop()!;
+        console.log(current);
+
+        const neighbors = this.getNeighbors(current);
+        if (neighbors) {
+          for (let i = 0; i < neighbors.length; i++) {
+            stack.push(neighbors[i]);
+          }
+        }
+      }
+    }
+
+    printBFS(sourceNodeValue: string) {
+      const queue = [sourceNodeValue];
+
+      while (queue.length > 0) {
+        const current = queue.shift()!;
+        console.log(current);
+
+        const neighbors = this.getNeighbors(current);
+
+        if (neighbors) {
+          for (let i = 0; i < neighbors.length; i++) {
+            queue.push(neighbors[i]);
+          }
+        }
+      }
+    }
+  }
+
+  const myGraph = new Graph();
+  myGraph.addNode("A");
+  myGraph.addNode("B");
+  myGraph.addNode("C");
+  myGraph.addNode("D");
+  myGraph.addNode("E");
+  myGraph.addNode("F");
+
+  myGraph.addEdge("A", "C");
+  myGraph.addEdge("A", "B");
+  myGraph.addEdge("C", "F");
+  myGraph.addEdge("D", "E");
+  myGraph.addEdge("B", "D");
+
+  console.log(myGraph.getAll());
+  // myGraph.printDFS("A");
+  myGraph.printBFS("A");
+}
